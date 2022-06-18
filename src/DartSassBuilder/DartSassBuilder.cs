14810803f7586c7622239b8c09fe98c41744b596
Dean <dean.dean.w@gmail.com>
2022-06-18 16:30:22 +0200

    Refactor main/entry point

---
+++ src/DartSassBuilder/DartSassBuilder.cs
@@ -0,0 +1,143 @@
+﻿using CommandLine;
+using DartSassHost;
+using DartSassHost.Helpers;
+using JavaScriptEngineSwitcher.V8;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DartSassBuilder
+{
+    public class DartSassBuilder
+    {
+        private readonly Parser _parser;
+
+        private GenericOptions _options;
+
+        public DartSassBuilder(Parser parser)
+        {
+            _parser = parser;
+        }
+
+        public async Task ParseArgumentsAsync(string[] args)
+        {
+            await _parser
+               .ParseArguments<DirectoryOptions, FilesOptions>(args)
+               .WithNotParsed(e => Environment.Exit(1))
+               .WithParsedAsync(async options =>
+               {
+                   switch (options)
+                   {
+                       case DirectoryOptions directory:
+                           {
+                               _options = directory;
+
+                               WriteLine($"Sass compile directory: {directory.Directory}");
+
+                               await CompileDirectoriesAsync(directory.Directory, directory.ExcludedDirectories);
+
+                               WriteLine("Sass files compiled");
+                           }
+                           break;
+                       case FilesOptions file:
+                           {
+                               _options = file;
+
+                               WriteLine($"Sass compile files");
+
+                               await CompileFilesAsync(file.Files);
+
+                               WriteLine("Sass files compiled");
+                           }
+                           break;
+                       default:
+                           throw new NotImplementedException("Invalid commandline option parsing");
+                   }
+               });
+        }
+
+        public async Task CompileDirectoriesAsync(string directory, IEnumerable<string> excludedDirectories)
+        {
+            var sassFiles = Directory.EnumerateFiles(directory)
+                .Where(file => file.EndsWith(".scss", StringComparison.OrdinalIgnoreCase) || file.EndsWith(".sass", StringComparison.OrdinalIgnoreCase));
+
+            await CompileFilesAsync(sassFiles);
+
+            var subDirectories = Directory.EnumerateDirectories(directory);
+            foreach (var subDirectory in subDirectories)
+            {
+                var directoryName = new DirectoryInfo(subDirectory).Name;
+                if (excludedDirectories.Any(dir => string.Equals(dir, directoryName, StringComparison.OrdinalIgnoreCase)))
+                    continue;
+
+                await CompileDirectoriesAsync(subDirectory, excludedDirectories);
+            }
+        }
+
+        public async Task CompileFilesAsync(IEnumerable<string> sassFiles)
+        {
+            try
+            {
+                using var sassCompiler = new SassCompiler(() => new V8JsEngineFactory().CreateEngine());
+
+                foreach (var file in sassFiles)
+                {
+                    var fileInfo = new FileInfo(file);
+                    if (fileInfo.Name.StartsWith("_"))
+                    {
+                        WriteVerbose($"Skipping: {fileInfo.FullName}");
+                        continue;
+                    }
+
+                    WriteVerbose($"Processing: {fileInfo.FullName}");
+
+                    var result = sassCompiler.CompileFile(file, options: _options.SassCompilationOptions);
+
+                    var newFile = fileInfo.FullName.Replace(fileInfo.Extension, ".css");
+
+                    if (File.Exists(newFile) && result.CompiledContent == await File.ReadAllTextAsync(newFile))
+                        continue;
+
+                    await File.WriteAllTextAsync(newFile, result.CompiledContent);
+                }
+            }
+            catch (SassCompilerLoadException e)
+            {
+                Console.WriteLine("During loading of Sass compiler an error occurred. See details:");
+                Console.WriteLine();
+                Console.WriteLine(SassErrorHelpers.GenerateErrorDetails(e));
+            }
+            catch (SassCompilationException e)
+            {
+                Console.WriteLine("During compilation of SCSS code an error occurred. See details:");
+                Console.WriteLine();
+                Console.WriteLine(SassErrorHelpers.GenerateErrorDetails(e));
+            }
+            catch (SassException e)
+            {
+                Console.WriteLine("During working of Sass compiler an unknown error occurred. See details:");
+                Console.WriteLine();
+                Console.WriteLine(SassErrorHelpers.GenerateErrorDetails(e));
+            }
+        }
+
+        public void WriteLine(string line)
+        {
+            if (_options.OutputLevel >= OutputLevel.Default)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        public void WriteVerbose(string line)
+        {
+            if (_options.OutputLevel >= OutputLevel.Verbose)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
